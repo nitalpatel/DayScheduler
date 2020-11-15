adac0360e510aadda42167d04e4e52d5f4d27e80
nitalpatel <72319223+nitalpatel@users.noreply.github.com>
2020-11-14 22:49:53 -0600

    Add day Schedule

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
+$(document).ready(function () {// basically  tells engine to load 1)html & 2)css first.  ($=jquery)
     //display current day & time.
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
@@ -9,18 +9,26 @@ $(document).ready(function () {// essentially tells engine to load 1)html & 2)cs
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        //set items in local storage.
+        //place items in local storage.
         localStorage.setItem(time, text);
     })
-    //load any saved data from LocalStorage - do this for each hour created.
+            //load any saved data from LocalStorage - do this for each hour created.
     $("#hour9 .description").val(localStorage.getItem("hour9"));
+
     $("#hour10 .description").val(localStorage.getItem("hour10"));
+
     $("#hour11 .description").val(localStorage.getItem("hour11"));
+
     $("#hour12 .description").val(localStorage.getItem("hour12"));
+
     $("#hour13 .description").val(localStorage.getItem("hour13"));
+
     $("#hour14 .description").val(localStorage.getItem("hour14"));
+
     $("#hour15 .description").val(localStorage.getItem("hour15"));
+
     $("#hour16 .description").val(localStorage.getItem("hour16"));
+
     $("#hour17 .description").val(localStorage.getItem("hour17"));
 
 
